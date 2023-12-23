4c6b09a060c01de0df1eccf093bbc8c59f19d5bb
adrianojcgit <48298141+adrianojcgit@users.noreply.github.com>
2023-12-23 08:53:40 -0300

    #Implementando o for basico

---
+++ AssincronaThreads/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿internal class Pessoa
+{
+    private string v1;
+    private string v2;
+    private int v3;
+
+    public Pessoa(string v1, string v2, int v3)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
+}
--- AssincronaThreads/Program.cs
+++ AssincronaThreads/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        RealizarOperacao(1, "Adriano", "Cordeiro");
+        sw.Stop();
+        Console.WriteLine($"A contagem demorou {sw.ElapsedMilliseconds} segundos");
+    }
+
+    private static void RealizarOperacao(int op, string nome, string segundoNome)
+    {
+        Console.WriteLine($"Iniciando operação {op}...");
+        for (int i = 0; i < 1000000000; i++)
+        {
+            var p = new Pessoa(nome, segundoNome, 30);
+        }
+        Console.WriteLine($"Operação {op} finalizada!");
     }
 }
