7f723476e35e7a9c794f0cf3b4af5d986031154a
adrianojcgit <48298141+adrianojcgit@users.noreply.github.com>
2023-12-23 09:18:59 -0300

    Adicionar trheads

--- AssincronaThreads/Program.cs
+++ AssincronaThreads/Program.cs
@@ -6,12 +6,42 @@ internal class Program
     {
         Stopwatch sw = new Stopwatch();
         sw.Start();
-        RealizarOperacao(1, "Adriano", "Cordeiro");
+        ExecutarComThreads();
         sw.Stop();
         Console.WriteLine($"A contagem demorou {sw.ElapsedMilliseconds} segundos");
     }
 
-    private static void RealizarOperacao(int op, string nome, string segundoNome)
+    static void ExecutarSequencial()
+    {
+        RealizarOperacao(1, "Adriano", "Cordeiro");
+        RealizarOperacao(2, "Sandra", "Pinheiro");
+        RealizarOperacao(3, "Fabio", "Alencar");
+    }
+
+    static void ExecutarComThreads()
+    {
+        var t1 = new Thread(() =>
+        {
+            RealizarOperacao(1, "Adriano", "Cordeiro");
+        });
+        var t2 = new Thread(() =>
+        {
+            RealizarOperacao(2, "Sandra", "Pinheiro");
+        });
+        var t3 = new Thread(() =>
+        {
+            RealizarOperacao(3, "Fabio", "Alencar");
+        });
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+    }
+    static void RealizarOperacao(int op, string nome, string segundoNome)
     {
         Console.WriteLine($"Iniciando operação {op}...");
         for (int i = 0; i < 1000000000; i++)
